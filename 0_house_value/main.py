7e674aea7faae59d8ed6196690ea88b70bc39762
DR E <31527345+erdeq-upenn@users.noreply.github.com>
2022-08-06 21:25:54 -0400

    init

---
+++ 0_house_value/main.py
@@ -0,0 +1,16 @@
+import numpy as py
+import sys
+import time
+
+def main():
+    t0 = time.time()
+    p0 = int(sys.argv[2])
+    year = 2022- int(sys.argv[1])
+    p1 = 1.03**year*p0
+    p2 = 1.05**year*p0
+    print("True value of House in 2022 is\nbetween %.2f and %.2f"%(p1,p2))
+    t1 = time.time()
+    print('time used %.2f ms'%((t1-t0)*1000))
+
+if __name__ == '__main__':
+    main()
