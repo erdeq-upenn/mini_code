507f506a90f90a141173eaa644e72d931767ae46
DR E <31527345+erdeq-upenn@users.noreply.github.com>
2022-08-08 23:09:53 -0400

    commited from Mac-Mon Aug  8 23:09:53 EDT 2022

--- 0_house_value/main.py
+++ 0_house_value/main.py
@@ -1,6 +1,7 @@
 import numpy as py
 import sys
 import time
+import os
 
 def main():
     t0 = time.time()
@@ -11,7 +12,7 @@ def main():
     print("True value of House in 2022 is\nbetween %.2f and %.2f"%(p1,p2))
     t1 = time.time()
     print('time used %.2f ms'%((t1-t0)*1000))
-    
 
 if __name__ == '__main__':
+    LOG_DIR = '/log'
     main()
