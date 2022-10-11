7f6393d8f754a80b9f5d96a01128cd23929b1525
DR E <31527345+erdeq-upenn@users.noreply.github.com>
2022-10-11 15:12:00 -0400

    auto-commit from myMac 2022-10-11-15:12:00

---
+++ 1_financial_python/util.py
@@ -0,0 +1,12 @@
+import numpy as np 
+import pandas as pd
+from pylab import plt,mpl
+
+import math 
+import scipy.stats as scs 
+import statsmodels.api as sm 
+
+# plt.style.use('seaborn')
+# mpl.rcParams['font.family']='serif'
+
+# %matplotlib inline
