d0d60ccc9c5b4eacb0a9227a1aca4e8d77aae951
dequan Linux desktop <tdscad123@hotmail.com>
2024-02-26 10:38:51 -0500

    auto-update from HOME-Desktop 2024-02-26-10:38:51

--- youtube_downloader/run.sh
+++ youtube_downloader/run.sh
@@ -9,13 +9,13 @@
 # ----------------------------------------------------------------------------------------
 ##     Wei
 # ----------------------------------------------------------------------------------------
-python get_transcripts.py --id "Tl2td45qmJo"  --log_dir='log' --data_dir='data' --mode='v'
-python get_transcripts.py --id "2Nv2VB7BHVI"  --log_dir='log' --data_dir='data' --mode='v'
-python get_transcripts.py --id "ZTTHwPsWT-I"  --log_dir='log' --data_dir='data' --mode='v'
-python get_transcripts.py --id "3i2D6-hkOss"  --log_dir='log' --data_dir='data' --mode='v'
-python get_transcripts.py --id "EyeI1rwlIHw"  --log_dir='log' --data_dir='data' --mode='v'
-python get_transcripts.py --id "icY03PR1CWI"  --log_dir='log' --data_dir='data' --mode='v'
-python get_transcripts.py --id "FVQmzsZ3Zq8"  --log_dir='log' --data_dir='data' --mode='v'
-python get_transcripts.py --id "aQaq7zvRS5Q"  --log_dir='log' --data_dir='data' --mode='v'
-python get_transcripts.py --id "YR7I7YYFbN0"  --log_dir='log' --data_dir='data' --mode='v'
-python get_transcripts.py --id "Pg4BwP9H1_4"  --log_dir='log' --data_dir='data' --mode='v'
+#python3 get_transcripts.py --id "Tl2td45qmJo"  --log_dir='log' --data_dir='data' --mode='v'
+python3 get_transcripts.py --id "2Nv2VB7BHVI"  --log_dir='log' --data_dir='data' --mode='v'
+#python get_transcripts.py --id "ZTTHwPsWT-I"  --log_dir='log' --data_dir='data' --mode='v'
+#python get_transcripts.py --id "3i2D6-hkOss"  --log_dir='log' --data_dir='data' --mode='v'
+#python get_transcripts.py --id "EyeI1rwlIHw"  --log_dir='log' --data_dir='data' --mode='v'
+#python get_transcripts.py --id "icY03PR1CWI"  --log_dir='log' --data_dir='data' --mode='v'
+#python get_transcripts.py --id "FVQmzsZ3Zq8"  --log_dir='log' --data_dir='data' --mode='v'
+#python get_transcripts.py --id "aQaq7zvRS5Q"  --log_dir='log' --data_dir='data' --mode='v'
+#python get_transcripts.py --id "YR7I7YYFbN0"  --log_dir='log' --data_dir='data' --mode='v'
+#python3 get_transcripts.py --id "Pg4BwP9H1_4"  --log_dir='log' --data_dir='data' --mode='v'
