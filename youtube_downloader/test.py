3c1fb13cf9f30ac6d4b5a148e7160d999d98fb24
DR E <31527345+erdeq-upenn@users.noreply.github.com>
2025-05-20 09:48:51 -0400

    auto-commit from myMac 2025-05-20-09:48:51

--- youtube_downloader/test.py
+++ youtube_downloader/test.py
@@ -16,7 +16,7 @@ from pytubefix.cli import on_progress
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-url = "https://www.youtube.com/watch?v=xEEYZLKgVTw"
+url = "https://www.youtube.com/watch?v=ekr2nIex040"
 
 yt = YouTube(url, on_progress_callback=on_progress)
 print(yt.title)
